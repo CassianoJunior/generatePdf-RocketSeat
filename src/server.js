056e2cd9d9427cfa4b904d8627488b8df9cfc299
Cassiano Junior <cassianojuniorww@gmail.com>
2022-05-26 21:06:46 -0300

    using puppeteer

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-const pdf = require("html-pdf");
+// const pdf = require("html-pdf");
+const puppeteer = require("puppeteer");
 const app = express();
 
 const passengers = [
@@ -22,6 +23,31 @@ const passengers = [
   },
 ];
 
+app.get("/pdf", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/", {
+    waitUntil: "networkidle0",
+  });
+
+  const pdf = await page.pdf({
+    printBackground: true,
+    format: "A4",
+    margin: {
+      top: "20px",
+      bottom: "40px",
+      left: "20px",
+      right: "20px",
+    },
+  });
+
+  await browser.close();
+
+  res.contentType("application/pdf");
+
+  return res.send(pdf);
+});
+
 app.get("/", (req, res) => {
   const filePath = path.join(__dirname, "print.ejs");
   ejs.renderFile(filePath, { passengers }, (err, data) => {
@@ -29,23 +55,26 @@ app.get("/", (req, res) => {
       return res.send("Erro na leitura do arquivo");
     }
 
-    const options = {
-      height: "11.25in",
-      width: "8.5in",
-      header: {
-        height: "20mm",
-      },
-      footer: {
-        height: "20mm",
-      },
-    };
+    // const options = {
+    //   height: "11.25in",
+    //   width: "8.5in",
+    //   header: {
+    //     height: "20mm",
+    //   },
+    //   footer: {
+    //     height: "20mm",
+    //   },
+    // };
 
-    pdf.create(data, options).toFile("report.pdf", (err, dataPdf) => {
-      if (err) {
-        return res.send("Erro ao gerar o PDF");
-      }
-      res.send(data);
-    });
+    // pdf.create(data, options).toFile("report.pdf", (err, dataPdf) => {
+    //   if (err) {
+    //     return res.send("Erro ao gerar o PDF");
+    //   }
+
+    //   res.send(data);
+    // });
+
+    res.send(data);
   });
 });
 
